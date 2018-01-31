29d657a1bcf8735c18bd53d26e0ff6b83f388af2
cyrillelenou <cyrille.lenou.001@student.uni.lu>
2018-01-31 23:01:24 +0100

    Global improvements concerning Checkin/checkout, event management and user links!

--- app/src/main/java/com/uni/redcarpet/ui/activities/MainActivity.java
+++ app/src/main/java/com/uni/redcarpet/ui/activities/MainActivity.java
@@ -1,21 +1,24 @@
 package com.uni.redcarpet.ui.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
